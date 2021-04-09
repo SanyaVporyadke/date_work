7844e6f8641b2dc95ff06f972cc37c9521232c12
Alexandra <alya.romanovna@mail.ru>
2021-04-09 22:33:56 +0300

    fourth ex

--- date_work.py
+++ date_work.py
@@ -20,4 +20,21 @@ def correct_date(li):
         else:
             print(False)
 
-correct_date(stream)
+#3 задание. Напишите функцию date_range, которая возвращает список дат за период от start_date до end_date. Даты должны вводиться в формате YYYY-MM-DD.
+#  В случае неверного формата или при start_date > end_date должен возвращаться пустой список.
+
+# 4 задание.
+
+DEFAULT_USER_COUNT = 3
+
+def delete_and_return_last_user(region, default_list=['A100', 'A101', 'A102']):
+#Удаляет из списка default_list последнего пользователя и возвращает ID нового последнего пользователя.
+    element_to_delete = default_list[-1]
+    default_list.remove(element_to_delete)
+    return default_list[-2]
+
+print(delete_and_return_last_user(1))
+print(delete_and_return_last_user(1))
+# ответ: в списке 3 элемента, при удалении последнего остаюся элементы с индексами -2(А101) и -3(А100),
+# но при повторном вызове функции A101 становится -1, а А100 -2, сначала происходит удаление и затем попытка вернуть значение с индексом -2,
+# но тогда А100 остается с индексом -1, а элемента с индексом -2 уже просто не существует.
